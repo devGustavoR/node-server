9c0a281cf5015be5419e6c673397077ef8e78264
devgustavor <devgustavor@gmail.com>
2023-09-25 13:48:55 -0300

    Instalação do Fastify e teste

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-import { createServer } from 'node:http';
+// import { createServer } from 'node:http';
 
-const server = createServer((request, response) =>{
-  response.write("Foi");
-  return response.end()
+// const server = createServer((request, response) =>{
+//   response.write("Foi");
+//   return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', () =>{
+  return 'Hello World'
 })
 
-server.listen(3333)
+server.get('/hello', () =>{
+  return 'Hello, World'
+})
+
+server.get('/wuors', () =>{
+  return 'Hello Wuors'
+})
+
+server.listen({
+  port: 3333,
+})
